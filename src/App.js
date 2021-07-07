58bcd59c1218f0ea04a72d592118811cdef8ffa1
DarcyJorgensen <darcy.jorgensen_23@yahoo.com>
2021-07-07 14:46:22 -0500

    update Login component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import Login from "./components/Login";
+import "./App.css";
+import { BrowserRouter as Router, NavLink } from "react-router-dom";
 
-function App() {
+function App({ username, clearSavedTrips, clearSearch, clearUser }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="flex-wrap">
+        {!username && (
+          <NavLink
+            activeClassName="active"
+            className="link text-center"
+            to="/login"
+          >
+            Login
+          </NavLink>
+        )}
+        {username && (
+          <>
+            <NavLink
+              activeClassName="active"
+              className="link text-center"
+              to="/search"
+            >
+              Search
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              className="link text-center"
+              to="/savedtrips"
+            >
+              Saved Trips
+            </NavLink>
+
+            <NavLink
+              className="link text-center"
+              to="/login"
+              onClick={() => {
+                clearSavedTrips();
+                clearSearch();
+                clearUser();
+              }}
+            >
+              Logout
+            </NavLink>
+          </>
+        )}
+      </nav>
+    </Router>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { setUser } from "../redux/actions";
+
+const Login = ({ setUser }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <h3 className="text-center">Login</h3>
+      <form className="form">
+        <div className="form-field flex-wrap">
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            placeholder="Eneter username at least 6 characters"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+        </div>
+
+        <div className="form-field flex-wrap">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Minimum of 6 characters"
+            onchange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+
+        <button
+          className="button"
+          onClick={(e) => {
+            e.preventDefault();
+            if (username.length > 5 && password.length > 4) {
+              setUser(username);
+            }
+          }}
+        >
+          Login
+        </button>
+      </form>
+    </>
+  );
+};
+
+function mapStateToProps(state) {
+  return {};
+}
+
+const mapDispatchToProps = {
+  setUser,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
